4219c2897a3f80ebf4a94568aa77f5150aff75fa
Jerry Mathew <jer2ymatheww@gmail.com>
2025-12-02 20:24:13 +0530

    backend initialized and db connected

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+// backend/src/app.module.ts
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // loads .env
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: config.getOrThrow<number>('DB_PORT'),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+
+  ],
+})
+export class AppModule { }
